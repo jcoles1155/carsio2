afec136b28137d40a3c2a67362f96b3a7d79a4e1
JC Coles <jcoles1155@gmail.com>
2021-04-29 11:26:36 -0700

    initial setup

---
+++ main_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# from .models import CarPost
+
+
+class Car:  # Note that parens are optional if not inheriting from another class
+    def __init__(self, manufacturer, year, carModel, color, body, isAvailable):
+        self.manufacturer = manufacturer
+        self.year = year
+        self.carModel = carModel
+        self.color = color
+        self.body = body
+
+cars = [
+    Car('Ford', '2011', 'F150', 'Grey', 'Crew Cab', True),
+    Car('Toyota', '2016', 'Tundra', 'Cobalt', 'Super Cab', True),
+    Car('Mercedes', '2017', 'S-Class', 'Blue',  'Coupe', False)
+]
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# # route for cars index
+# def cars_index(request):
+#     cars = CarPost.objects.all()
+#     return render(request, 'cars/index.html', { 'cars': cars })
+
+# def cars_detail(request, car_id):
+#     car = CarPost.objects.get(id=car_id)
+#     return render(request, 'cars/detail.html', { 'car': car })
